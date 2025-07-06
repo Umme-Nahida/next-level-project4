76355bd2802ed202818b83955ba5eca88690d2a9
Developer Nahida <ummenahida20@gmail.com>
2025-07-07 05:17:06 +0600

    added backend base api

--- src/features/api/baseApi.ts
+++ src/features/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
     reducerPath: "baseApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://assignment3-gamma-wheat.vercel.app/api" }),
     tagTypes: ['book','borrow'],
     endpoints: (builder) => ({
         getBooks: builder.query({
